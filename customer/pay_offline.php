98bb54c494983a810b03c03e96df847e1f4b8d64
chamlingsmriti-git <ismritichamling538@gmail.com>
2024-06-21 14:05:16 +0545

    first commit

---
+++ customer/pay_offline.php
@@ -0,0 +1,56 @@
+<center><!--  center Begin  -->
+    
+
+    <h1> Pay Offline Using Method</h1>
+    
+ 
+    
+    <p class="text-muted">
+        
+        If you have any questions, feel free to <a href="../contact.php">Contact Us</a>. Our Customer Service work <strong>24/7</strong>
+        
+    </p>
+    
+</center><!--  center Finish  -->
+
+
+<div class="table-responsive"><!--  table-responsive Begin  -->
+    
+    <table class="table table-bordered table-hover table-striped"><!--  table table-bordered table-hover Begin  -->
+        
+        <thead><!--  thead Begin  -->
+            
+            <tr><!--  tr Begin  -->
+
+                <th>Bank Account Details:</th>
+                <th>Account owner:</th>
+                <th>Amount</th>
+
+                
+            </tr><!--  tr Finish  -->
+            
+        </thead><!--  thead Finish  -->
+        
+        <tbody><!--  tbody Begin  -->
+
+        <td>Bank Name: NISASIA | Account No: 1801820838 | Branch Name:Bratchowk | Branch Code:1498</td>
+        <td>NIC #98485-545994| Mobile no:981263746|Name:MrGhie</td>
+        <td>Real Name:Mr Kapil Tamang|Mobile no:981263746</td>
+            
+            <tr><!--  tr Begin  -->
+                
+            </tr><!--  tr Finish  -->
+            
+            <tr><!--  tr Begin  -->
+                
+            </tr><!--  tr Finish  -->
+            
+            <tr><!--  tr Begin  -->
+
+            </tr><!--  tr Finish  -->
+            
+        </tbody><!--  tbody Finish  -->
+        
+    </table><!--  table table-bordered table-hover Finish  -->
+    
+</div><!--  table-responsive Finish  -->
